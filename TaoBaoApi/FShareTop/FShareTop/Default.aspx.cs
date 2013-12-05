cfca9fabdf6b07f258a92a5e19ccd88e0f3330f6
JayCao <mcgrady_cao@163.com>
2013-12-05 16:57:49 +0800

    add project success
    add project

---
+++ TaoBaoApi/FShareTop/FShareTop/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace FShareTop
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        public string Appkey = "21694347", Appsecret = "9ecf05d134515995894d2aefed732b17";
+        public string sxAppkey = "1021694347", sxAppsecret = "sandbox134515995894d2aefed732b17";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
